8daec56df6cae347c55b1e916dba801df2fee5e6
Domagoj <paulic.domagoj@gmail.com>
2023-07-15 11:07:37 +0200

    add quote

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Page } from './components/page/page'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css';
+import { Page } from './components/page/page';
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
+  const quote = [];
 
   return (
     <Page>
-      <div>
+      {/*  <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,9 +29,25 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <blockquote>
+        <p>
+          Everything written symbols can say has already passed by. They are
+          like tracks left by animals. That is why the masters of meditation
+          refuse to accept that writings are final. The aim is to reach true
+          being by means of those tracks, those letters, those signs - but
+          reality itself is not a sign, and it leaves no tracks. It doesn’t come
+          to us by way of letters or words. We can go towards it, by following
+          those words and letters back to what they came from. But so long as we
+          are preoccupied with symbols, theories and opinions, we will fail to
+          reach the principle.
+        </p>
+        <p>But when we give up symbols and opinions, aren’t we left with the utter nothingness of being?</p>
+        <p>Yes.</p>
+      </blockquote>
+      <figcaption>— Kimura Kyuho, <cite>Kenjutsu Fushigi Hen [On the Mysteries of Swordsmanship], 1768</cite></figcaption>
     </Page>
-  )
+  );
 }
 
-export default App
+export default App;
