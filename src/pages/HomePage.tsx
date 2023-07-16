5829c2ad170a39fc1f8a9adbf0e34adef7ad9a30
Domagoj <paulic.domagoj@gmail.com>
2023-07-16 12:43:57 +0200

    add image, amend styles

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,35 +1,47 @@
 import { Page } from '../components/Page/Page';
 import { useRouter } from '../router/RouterContext';
+import coverImage from '../assets/the_daily_troll_cover_img.jpg';
+import styled from 'styled-components';
 
 export const HomePage = () => {
-  const {navigate} = useRouter()
+  const { navigate } = useRouter();
   return (
     <Page>
-      <blockquote>
-        <p>
-          Everything written symbols can say has already passed by. They are
-          like tracks left by animals. That is why the masters of meditation
-          refuse to accept that writings are final. The aim is to reach true
-          being by means of those tracks, those letters, those signs - but
-          reality itself is not a sign, and it leaves no tracks. It doesn’t come
-          to us by way of letters or words. We can go towards it, by following
-          those words and letters back to what they came from. But so long as we
-          are preoccupied with symbols, theories and opinions, we will fail to
-          reach the principle.
-        </p>
-        <p>
-          But when we give up symbols and opinions, aren’t we left with the
-          utter nothingness of being?
-        </p>
-        <p>Yes.</p>
-      </blockquote>
-      <figcaption>
-        — Kimura Kyuho,{' '}
-        <cite>
-          Kenjutsu Fushigi Hen [On the Mysteries of Swordsmanship], 1768
-        </cite>
-      </figcaption>
-      <button onClick={() => navigate('/react')}>React home page</button>
+      <Wrapper>
+        <img src={coverImage} />
+        <blockquote>
+          <p>
+            Everything written symbols can say has already passed by. They are
+            like tracks left by animals. That is why the masters of meditation
+            refuse to accept that writings are final. The aim is to reach true
+            being by means of those tracks, those letters, those signs - but
+            reality itself is not a sign, and it leaves no tracks. It doesn’t
+            come to us by way of letters or words. We can go towards it, by
+            following those words and letters back to what they came from. But
+            so long as we are preoccupied with symbols, theories and opinions,
+            we will fail to reach the principle.
+          </p>
+          <p>
+            But when we give up symbols and opinions, aren’t we left with the
+            utter nothingness of being?
+          </p>
+          <p>Yes.</p>
+        </blockquote>
+        <figcaption>
+          — Kimura Kyuho,{' '}
+          <cite>
+            Kenjutsu Fushigi Hen [On the Mysteries of Swordsmanship], 1768
+          </cite>
+        </figcaption>
+        <button onClick={() => navigate('/react')}>React home page</button>
+      </Wrapper>
     </Page>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: stretch;
+`;
